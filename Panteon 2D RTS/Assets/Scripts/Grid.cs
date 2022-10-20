1d683739305ff6eff1a15d8ae753156160fb170f
atilladuruel <40753622+atilladuruel@users.noreply.github.com>
2022-10-21 02:40:55 +0300

    Map, UI adding

---
+++ Panteon 2D RTS/Assets/Scripts/Grid.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid 
+{
+    private int width;
+    private int height;
+    private int[,] gridArray;
+
+    public Grid(int width,int height)
+    {
+        this.width = width;
+        this.height = height;
+
+        this.gridArray = new int[width, height];
+
+    }
+}
